dac13f2edc03bae922187f213ae5e29abe10547d
unknown <brandenek03912@hotmail.com>
2024-02-20 18:49:01 -0500

    init: add base project files

---
+++ Blasphemous.Framework.UI/Main.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace Blasphemous.Framework.UI;
+
+[BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
+[BepInDependency("Blasphemous.ModdingAPI", "2.1.0")]
+internal class Main : BaseUnityPlugin
+{
+    public static UIFramework UIFramework { get; private set; }
+
+    private void Start()
+    {
+        UIFramework = new UIFramework();
+    }
+}
---
+++ Blasphemous.Framework.UI/UIFramework.cs
@@ -0,0 +1,16 @@
+﻿using Blasphemous.ModdingAPI;
+
+namespace Blasphemous.Framework.UI;
+
+/// <summary>
+/// Handles initializing and creating UI
+/// </summary>
+public class UIFramework : BlasMod
+{
+    internal UIFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        LogError($"{ModInfo.MOD_NAME} has been initialized");
+    }
+}
